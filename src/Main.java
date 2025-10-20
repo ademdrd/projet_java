e0306999962398ee3788c013984463a955b24845
ademdrd <adem.drd11@gmail.com>
2025-10-20 22:18:46 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int age;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("quelle est votre age : ");
+        public  age= sc.nextInt();
+
+        System.out.println("votre age est "+age+" ans.");
+
+
+
+
+    }
+}
